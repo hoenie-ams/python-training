2df5fefa38a8ddf8bf2fb7a241319a8a92bc6191
BrightBoost <info@brightboost.nl>
2022-02-04 12:18:08 +0100

    demo fastapi

---
+++ src/simple_api.py
@@ -0,0 +1,57 @@
+"""
+Example of FastAPI app
+"""
+
+from fastapi import FastAPI, HTTPException
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+app = FastAPI()
+
+
+class Product(SQLModel, table=True):
+    __tablename__ = "products"
+    id: int = Field(primary_key=True)
+    name: str = Field(default=None)
+    price: int = Field(default=None)
+
+
+sqlite_url = "sqlite:///database.db"
+engine = create_engine(sqlite_url, echo=True, connect_args={"check_same_thread": False})
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello World"}
+
+
+@app.get("/products/")
+def read_products():
+    with Session(engine) as session:
+        products = session.exec(select(Product)).all()
+        return products
+
+
+@app.get("/products/{product_id}")
+def read_product(product_id: int):
+    with Session(engine) as session:
+        product = session.get(Product, product_id)
+        if not product:
+            raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+@app.post("/products/")
+def create_product(product: Product):
+    with Session(engine) as session:
+        session.add(product)
+        session.commit()
+        session.refresh(product)
+        return product
